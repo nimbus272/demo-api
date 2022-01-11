7e60b9103fe9aafd832ffb41081e073367728fc0
Dennis <nimbus272@gmail.com>
2022-01-10 23:06:56 -0500

    "push from vs code"

--- src/main/java/com/example/demoapi/controller/DemoController.java
+++ src/main/java/com/example/demoapi/controller/DemoController.java
@@ -9,7 +9,7 @@ public class DemoController {
 
     @RequestMapping(method = RequestMethod.GET, path = "/")
     public String demoEndpoint() {
-        return "bruh stop";
+        return "push test";
     }
     
 }
