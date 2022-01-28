c0fb9f51d54d7fb97274a333b5e5540aa4e8d4c5
Dennis Kerr <nimbus272@gmail.com>
2022-01-27 21:31:04 -0500

    adding custom expression evaluation

--- src/main/java/com/example/demoapi/Service/CalculatorService.java
+++ src/main/java/com/example/demoapi/Service/CalculatorService.java
@@ -15,14 +15,36 @@ public class CalculatorService {
         LOGGER.info("Reached runCalculator with expression recieved: {}",
                 objectNode.get("expression").asText());
         String expression = objectNode.get("expression").asText();
+        String newExpression = evaluateString(expression);
+        LOGGER.info("Removed parentheses from expression, new expression is : {}", newExpression);
 
         DoubleEvaluator eval = new DoubleEvaluator();
         try {
-            return eval.evaluate(expression);
+            return eval.evaluate(newExpression);
         } catch (IllegalArgumentException e) {
             LOGGER.error(e.getMessage());
             throw new NullPointerException();
         }
 
+    }
+
+    public String evaluateString(String expression) {
+        int lParen = -1;
+        int rParen = -1;
+        String subExpression = "";
+        String newExpression = "";
+        for (int i = 0; i < expression.length(); i++) {
+            if (expression.charAt(i) == '(') {
+                lParen = i;
+            } else if (expression.charAt(i) == ')') {
+                rParen = i;
+            }
+            if (lParen >= 0 && rParen >= 0) {
+                subExpression = expression.substring(lParen, rParen + 1);
+                newExpression = expression.replace(subExpression, "7");
+                
+            }
+        }
+        return newExpression;
     }
 }
