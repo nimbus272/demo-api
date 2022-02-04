b9fd7a6b6d6c403c5b1626f5e129b28e4e8567c3
Dennis Kerr <nimbus272@gmail.com>
2022-02-03 22:53:40 -0500

    added more robust custom string evaluation

--- src/main/java/com/example/demoapi/Service/CalculatorService.java
+++ src/main/java/com/example/demoapi/Service/CalculatorService.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class CalculatorService {
+    
     private static final Logger LOGGER = LoggerFactory.getLogger(CalculatorService.class);
 
     public CalculatorService() {
@@ -16,6 +17,7 @@ public class CalculatorService {
                 objectNode.get("expression").asText());
         String expression = objectNode.get("expression").asText();
         String newExpression = evaluateString(expression);
+        
         LOGGER.info("Removed parentheses from expression, new expression is : {}", newExpression);
 
         DoubleEvaluator eval = new DoubleEvaluator();
@@ -29,22 +31,40 @@ public class CalculatorService {
     }
 
     public String evaluateString(String expression) {
+        DoubleEvaluator eval = new DoubleEvaluator();
         int lParen = -1;
         int rParen = -1;
         String subExpression = "";
-        String newExpression = "";
+        String newExpression = expression;
         for (int i = 0; i < expression.length(); i++) {
             if (expression.charAt(i) == '(') {
                 lParen = i;
-            } else if (expression.charAt(i) == ')') {
+            } else if (expression.charAt(i) == ')'  && lParen >= 0) {
                 rParen = i;
             }
             if (lParen >= 0 && rParen >= 0) {
                 subExpression = expression.substring(lParen, rParen + 1);
-                newExpression = expression.replace(subExpression, "7");
+                
+                double solution = 0;
+                try {
+                    solution = eval.evaluate(subExpression);
+                    rParen = -1;
+                    lParen = -1;
+                } catch (Exception e) {
+                    LOGGER.error(e.getMessage());
+                }
+
+
+                newExpression = newExpression.replace(subExpression, String.valueOf(solution));
+
                 
             }
         }
+        if (newExpression.contains("(") || newExpression.contains(")")) {
+            newExpression = evaluateString(newExpression);
+        }
+
+
         return newExpression;
     }
 }
